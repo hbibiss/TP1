a27d739e3f12231a31fd398292e8fc751b86cfac
Imad <kalloubi.imad@gmail.com>
2020-01-14 16:04:32 +0100

    add var to script

--- Versionning-TP1/src/application.bat
+++ Versionning-TP1/src/application.bat
@@ -1,3 +1,9 @@
 ### application.bat - 2019 - Imad Kalloubi ###
 echo "version 1"
 echo "Build : 10:00"
+### application.bat - 201x - votre nom ###
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
